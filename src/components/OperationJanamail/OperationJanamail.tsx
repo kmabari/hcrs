bb72926e9f5c7e6cb9096ebdc609c2a2a8c02715
kmabari <kmabarikiyafoods@gmail.com>
2026-07-07 14:16:46 +0530

    Update OperationJanamail.tsx

--- src/components/OperationJanamail/OperationJanamail.tsx
+++ src/components/OperationJanamail/OperationJanamail.tsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+export default function OperationJanamail() {
+  return (
+    <div style={{ padding: "30px", textAlign: "center" }}>
+      <h1>🚀 OPERATION JANAMAIL</h1>
+
+      <h2>ജനങ്ങൾ ഉണർന്നു...</h2>
+
+      <h2>അധികാരികളേ, ഉണരൂ.</h2>
+
+      <p>Project Started Successfully.</p>
+    </div>
+  );
+}
